46f34bd0755469f37f0b0c49ef74c9c24c691729
Andrey Bezpalov <abezpalov@ya.ru>
2014-09-02 01:39:38 +0400

    Модули загрузчика загружаются по требованию

---
+++ updaters/ocs.py
@@ -0,0 +1,7 @@
+class Update():
+
+	name = 'OCS'
+	alias = 'ocs'
+
+	def __init__(self):
+		return true
--- views.py
+++ views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 
 # TODO времено?
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from catalog.models import Updater
 
 def index(request):
     return HttpResponse("Hello, world.")
 
 def updaters(request):
-    return HttpResponse("Список загрузчиков.")
+	updaters_list = Updater.objects.all()
+	context = {'updaters_list': updaters_list}
+	return render(request, 'catalog/updaters.html', context)
 
 def updater(request, alias):
-    return HttpResponse("Загрузчик %s." % alias)
+	return HttpResponse("Загрузчик %s." % alias)
 
 def update(request, alias):
-    return HttpResponse("Выполнение загрузчика %s." % alias)
 
+	Updater = __import__('catalog.updaters.' + alias, fromlist=['Update'])
+	return HttpResponse("Выполнение загрузчика %s." % Updater.Update.name)
