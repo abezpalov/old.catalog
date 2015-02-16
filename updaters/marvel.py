0aebed73e2e659416ea5a5eb439af03565d3cc47
Andrey Bezpalov <abezpalov@ya.ru>
2015-02-16 05:35:10 +0400

    #31 Updater Marvel

--- updaters/marvel.py
+++ updaters/marvel.py
@@ -1,3 +1,4 @@
+import requests
 from catalog.models import Updater
 from catalog.models import Distributor
 from catalog.models import Stock
@@ -20,56 +21,77 @@ class Runner:
 	def __init__(self):
 
 		# Объект дистрибьютора
-		self.distributor = Distributor.objects.take(alias=self.alias, name=self.name)
+		self.distributor = Distributor.objects.take(
+			alias = self.alias,
+			name  = self.name)
 
 		# Объект загрузчика
-		self.updater = Updater.objects.take(alias=self.alias, name=self.name, distributor=self.distributor)
+		self.updater = Updater.objects.take(
+			alias       = self.alias,
+			name        = self.name,
+			distributor = self.distributor)
 
-		# Склад
-		self.stock = Stock.objects.take(
-			alias = self.alias+'-stock',
-			name = self.name + ': склад',
+		# Склад в Москве
+		self.stock_msk = Stock.objects.take(
+			alias             = self.alias + '-stock-msk',
+			name              = self.name + ': склад в Москве',
 			delivery_time_min = 3,
 			delivery_time_max = 10,
-			distributor = self.distributor)
-		Party.objects.clear(stock=self.stock)
+			distributor       = self.distributor)
+		Party.objects.clear(stock = self.stock_msk)
 
-		# Ближний транзит
-		self.transit_b = Stock.objects.take(
-			alias = self.alias+'-b-transit',
-			name = self.name + ': ближний транзит',
-			delivery_time_min = 10,
-			delivery_time_max = 20,
-			distributor = self.distributor)
-		Party.objects.clear(stock=self.transit_b)
-
-		# Дальний транзит
-		self.transit_d = Stock.objects.take(
-			alias = self.alias+'-d-transit',
-			name = self.name + ': дальний транзит',
-			delivery_time_min = 20,
-			delivery_time_max = 60,
-			distributor = self.distributor)
-		Party.objects.clear(stock=self.transit_d)
+		# Склад в Санкт-Петербурге
+		self.stock_spb = Stock.objects.take(
+			alias             = self.alias + '-stock-spb',
+			name              = self.name + ': склад в Санкт-Петербурге',
+			delivery_time_min = 3,
+			delivery_time_max = 10,
+			distributor       = self.distributor)
+		Party.objects.clear(stock = self.stock_spb)
 
 		# Единица измерения
-		self.default_unit = Unit.objects.take(alias='pcs', name='шт.')
+		self.default_unit = Unit.objects.take(alias = 'pcs', name = 'шт.')
 
 		# Тип цены
 		self.dp = PriceType.objects.take(alias = 'DP', name = 'Диллерская цена')
 
 		# Валюты
-		self.rub = Currency.objects.take(alias = 'RUB', name = 'р.', full_name = 'Российский рубль', rate = 1, quantity = 1)
-		self.usd = Currency.objects.take(alias = 'USD', name = '$', full_name = 'US Dollar', rate = 60, quantity = 1)
-		self.eur = Currency.objects.take(alias = 'EUR', name = 'EUR', full_name = 'Euro', rate = 80, quantity = 1)
+		self.rub = Currency.objects.take(
+			alias = 'RUB',
+			name = 'р.',
+			full_name = 'Российский рубль',
+			rate = 1,
+			quantity = 1)
+		self.usd = Currency.objects.take(
+			alias = 'USD',
+			name = '$',
+			full_name = 'US Dollar',
+			rate = 60,
+			quantity = 1)
+		self.eur = Currency.objects.take(
+			alias = 'EUR',
+			name = 'EUR',
+			full_name = 'Euro',
+			rate = 80,
+			quantity = 1)
 
-		# Используемая ссылки
+		# Дополнительные переменные
 		self.url = 'https://b2b.marvel.ru/Api/'
 		self.key = ''
 		self.task = {
-			'categories': 'GetCatalogCategories'}
+			'categories': 'GetCatalogCategories',
+			'catalog':    'GetFullStock'}
 		self.request_format = {'xml': '0', 'json': '1'}
 		self.cookies = None
+		self.category_synonyms = {}
+		self.currencies = {
+			'RUB': self.rub,
+			'USD': self.usd,
+			'EUR': self.eur}
+		self.stocks = {
+			'msk': self.stock_msk,
+			'spb': self.stock_spb}
+
 
 	def run(self):
 
@@ -78,23 +100,39 @@ class Runner:
 			print('Ошибка: Проверьте параметры авторизации. Кажется их нет.')
 			return False
 
-		categories = self.getInfo('categories', 'json')
+		# Получаем категории для обработки
+		data = self.getData('categories', 'json')
 
+		# Обрабатываем категории
+		if data: self.parseCategories(data)
+		else: return False
 
+		# Получаем каталог для обработки
+		data = self.getData('catalog', 'json', 1)
 
-		# TODO Next
+		# Обрабатываем каталог
+		if data: self.parseCatalog(data)
+		else: return False
 
-		return False
+		return True
 
-	# Функция запроса к API дистрибьютора
-	def getInfo(self, task, request_format, number_of_attempts = 10):
+	def getData(self, task, request_format, pack_status = None):
 
 		# Создаем сессию
-		import requests
 		s = requests.Session()
 
-		# Получаем каталог
-		url = '{url}{task}?user={login}&password={password}&secretKey={key}&responseFormat={request_format}'.format(
+		# Собираем URL
+		if not pack_status is None:
+			url = '{url}{task}?user={login}&password={password}&secretKey={key}&packStatus={pack_status}&responseFormat={request_format}'.format(
+			url            = self.url,
+			task           = self.task[task],
+			login          = self.updater.login,
+			password       = self.updater.password,
+			key            = self.key,
+			pack_status    = pack_status,
+			request_format = self.request_format[request_format])
+		else:
+			url = '{url}{task}?user={login}&password={password}&secretKey={key}&responseFormat={request_format}'.format(
 			url            = self.url,
 			task           = self.task[task],
 			login          = self.updater.login,
@@ -102,17 +140,140 @@ class Runner:
 			key            = self.key,
 			request_format = self.request_format[request_format])
 
-		# TODO TEST
-		print(url)
+		# Выполняем запрос
+		r = s.post(
+			url,
+			cookies = self.cookies,
+			verify = False,
+			timeout = 300)
 
-		r = s.post(url, cookies = self.cookies, allow_redirects = True, verify = False, timeout = 60)
+		# Обрабатываем ответ
+		if 'json' == request_format:
+			import json
+			data = json.loads(r.text)
+			if data['Header']['Key']: self.key = data['Header']['Key']
+			if data['Header']['Code'] != 0:
+				print('Ошибка: невнятный ответ сервера.')
+				if data['Header']['Message']: print(data['Header']['Message'])
+				return False
+			else:
+				return data['Body']
+		else:
+			print('Ошибка: используется неподдерживаемый формат.')
+			return False
 
-		# TODO TEST
-		print(r.text[0:5000])
+
+	def parseCategories(self, data):
+
+		# Категории
+		for category in data['Categories']:
+			self.parseCategory(category)
 
 
+	def parseCategory(self, category):
 
-		# TODO Next
-		return False
+		# ID
+		category_id = category['CategoryID']
+		parent_id = category['ParentCategoryId']
+
+		# Имя
+		category_name = category['CategoryName']
+		if parent_id:
+			category_name = "{} | {}".format(
+				self.category_synonyms[parent_id],
+				category_name)
+
+		# Добавляем в словарь
+		self.category_synonyms[category_id] = category_name
+		print(category_name)
+
+		# Проходим рекурсивно по подкатегориям
+		for sub_category in category['SubCategories']:
+			self.parseCategory(sub_category)
 
 
+	def parseCatalog(self, data):
+
+		# Проходим по категориям
+		for item in data['Categories']:
+
+			# Определяем значение переменных
+			product_article       = item['WareArticle']
+			product_name          = item['WareFullName']
+			category_synonym_id   = item['CategoryId']
+			category_synonym_name = self.category_synonyms[category_synonym_id]
+			vendor_synonym_name   = item['WareVendor']
+
+			party_price    = self.fixPrice(item['WarePrice'])
+			party_currency = self.currencies[item['WareVendor']]
+			party_quantity = {
+				'msk': self.fixQuantity(item['AvailableForShippingInMSKCount']),
+				'spb': self.fixQuantity(item['AvailableForShippingInSPBCount'])}
+
+			# Синоним категории
+			category_synonym = CategorySynonym.objects.take(
+				name        = category_synonym_name,
+				updater     = self.updater,
+				distributor = self.distributor)
+
+			# Синоним производителя
+			vendor_synonym = VendorSynonym.objects.take(
+				name        = vendor_synonym_name,
+				updater     = self.updater,
+				distributor = self.distributor)
+
+			# Продукт
+			if product_article and product_name and vendor_synonym.vendor:
+				product = Product.objects.take(
+					article  = product_article,
+					vendor   = vendor_synonym.vendor,
+					name     = product_name,
+					category = category_synonym.category,
+					unit     = self.default_unit)
+				print("{} {}".format(product.vendor.name, product.article))
+
+			# Партии
+			stock_name = 'msk'
+			if party_quantity[stock_name]:
+				party = Party.objects.make(
+					product    = product,
+					stock      = self.stocks[stock_name],
+					price      = party_price,
+					price_type = self.dp,
+					currency   = party_currency,
+					quantity   = party_quantity[stock_name],
+					unit       = self.default_unit)
+				print("{} {} = {} {}".format(
+					product.vendor.name,
+					product.article,
+					party.price,
+					party.currency.alias))
+			stock_name = 'spb'
+			if party_quantity[stock_name]:
+				party = Party.objects.make(
+					product    = product,
+					stock      = self.stocks[stock_name],
+					price      = party_price,
+					price_type = self.dp,
+					currency   = party_currency,
+					quantity   = party_quantity[stock_name],
+					unit       = self.default_unit)
+				print("{} {} = {} {}".format(
+					product.vendor.name,
+					product.article,
+					party.price,
+					party.currency.alias))
+
+
+	def fixPrice(self, price):
+		price = str(price)
+		price = price.replace(',', '.')
+		if price: price = float(price)
+		else: price = None
+		return price
+
+
+	def fixQuantity(self, quantity):
+		price = price.replace('+', '')
+		quantity = int(quantity)
+		return quantity
