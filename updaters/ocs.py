402620f7d6923251d97779497066696a73159b7f
Andrey Bezpalov <abezpalov@ya.ru>
2015-04-14 21:22:42 +0300

    Update ocs.py

--- updaters/ocs.py
+++ updaters/ocs.py
@@ -61,6 +61,14 @@ class Runner:
 			distributor       = self.distributor)
 		Party.objects.clear(stock = self.stocks['Оренбург'])
 
+		self.stocks['Казань'] = Stock.objects.take(
+			alias             = self.alias + '-stock-kazan',
+			name              = self.name + ': склад в Казани',
+			delivery_time_min = 3,
+			delivery_time_max = 10,
+			distributor       = self.distributor)
+		Party.objects.clear(stock = self.stocks['Казань'])
+
 		self.stocks['Нижний Новгород'] = Stock.objects.take(
 			alias             = self.alias + '-stock-nizhniy-novgorod',
 			name              = self.name + ': склад в Нижнем Новгороде',
